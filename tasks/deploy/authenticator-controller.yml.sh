ab47cdf28f9a34062ede1d01d299cf1084517383
Raphael SIMON <simonrapha6l@gmail.com>
2015-09-08 10:56:28 +0200

    fix () to {}

--- tasks/deploy/authenticator-controller.yml.sh
+++ tasks/deploy/authenticator-controller.yml.sh
@@ -8,15 +8,15 @@ metadata:
   name: authenticator
   labels:
     name: authenticator
-    branch: $(CIRCLE_BRANCH)
-    commit: $(CIRCLE_SHA1)
+    branch: ${CIRCLE_BRANCH}
+    commit: ${CIRCLE_SHA1}
 spec:
-  replicas: $(REPLICAS_NUMBER)
+  replicas: ${REPLICAS_NUMBER}
   # selector identifies the set of pods that this
   # replication controller is responsible for managing
   selector:
     name: authenticator
-    branch: $(CIRCLE_BRANCH)
+    branch: ${CIRCLE_BRANCH}
   # template defines the 'cookie cutter' used for creating
   # new pods when necessary
   template:
@@ -25,13 +25,13 @@ spec:
         # Important: these labels need to match the selector above
         # The api server enforces this constraint.
         name: authenticator
-        branch: $(CIRCLE_BRANCH)
+        branch: ${CIRCLE_BRANCH}
     spec:
       containers:
       - name: authenticator
         command: ["npm", "start", "--"]
         args: ["--ws-url", "ws://crossbar-private:8081"]
-        image: eu.gcr.io/saio-fr/authenticator:$(CIRCLE_BRANCH)_$(CIRCLE_SHA1)
+        image: eu.gcr.io/saio-fr/authenticator:${CIRCLE_BRANCH}_${CIRCLE_SHA1}
         ports:
         - containerPort: 8081
 EOF
